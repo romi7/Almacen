6115c47d44eef2b5399ba17c68a7ec3a1a9c5fc5
bea y bequi <bea y bequi@DESKTOP-JDSSUAR>
2018-11-27 20:19:07 +0100

    ya tenemos creado el login y la base de datos terminado

--- index.php
+++ index.php
@@ -96,6 +96,7 @@ and open the template in the editor.
         
     </script>
     
+    
     <div class="modal fade" id="btnAnyadir" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
